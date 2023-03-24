4dbed77bde805b387e1d88e826170d608cbd7a92
RodrigoLopez19 <126137612+RodrigoLopez19@users.noreply.github.com>
2023-03-24 05:51:57 -0300

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,13 @@
+document.getElementById('mando').addEventListener('click',function(){
+    document.getElementById("mostrar").innerHTML="Juego FIFA y CS:GO"
+});
+document.getElementById('amigos').addEventListener('click',function(){
+    document.getElementById("mostrar_2").innerHTML="No tengo muchos amigos, solo los necesarios"
+});
+document.getElementById('pizza').addEventListener('click',function(){
+    document.getElementById("mostrar_3").innerHTML="Mi comida favorita es la pizza"
+});
+document.getElementById('despegue').addEventListener('click',function(){
+    document.getElementById("mostrar_4").innerHTML="Disfruto mucho irme de viaje"
+});
+
